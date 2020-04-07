b2f90bd63b59f98ce8146b7513002eb5c8230635
Jaroslav Psenicka <jaroslav.psenicka@gmail.com>
2020-04-07 21:55:44 +0200

    #2 cleanup

--- client/pages/ProductDetailPage.js
+++ client/pages/ProductDetailPage.js
@@ -1,51 +1,43 @@
 import React, { useContext } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Row from 'react-bootstrap/Row'
 import { ProductsContext } from '../ProductsContext';
 
 import Loading from '../components/Loading';
 import LoadingError from '../components/LoadingError';
-import Search from '../components/Search';
-import CaseOverview from '../components/CaseOverview';
-
-import styled from 'styled-components';
 
 const ProductDetailPage = ({ name }) => {
 
   const [ products ] = useContext(ProductsContext);
 
-  const ProductDetail = ({ product }) => {
-    return (
-      <div className="row">
-        <div className="col-md-3 text-secondary">Name</div>
-        <div className="col-md-9">{product.name}</div>
-        <div className="col-md-3 text-secondary">Label</div>
-        <div className="col-md-9">{product.label}</div>
-        <div className="col-md-3 text-secondary">Description</div>
-        <div className="col-md-9">{product.description}</div>
-        <div className="col-md-3 text-secondary">Icon URL</div>
-        <div className="col-md-9">{product.icon}</div>
-        <div className="col-md-3 text-secondary">Overview URL</div>
-        <div className="col-md-9">{product.overviewComponentUrl}</div>
-      </div>
-    )
-  }
-
   const Product = ({ name }) => {
     const product = products.data.find(p => p.name === name);
-    return product ? <>
+    if (!product) return <NoSuchProduct />;
+    return <>
       <h4 className="text-muted font-weight-light text-uppercase mb-4">{product.spec.label}</h4>
-      <ProductDetail product={product.spec} />
-    </> : null;
+      <div className="row">
+        <div className="col-md-3 text-secondary">Name</div>
+        <div className="col-md-9">{product.spec.name}</div>
+        <div className="col-md-3 text-secondary">Label</div>
+        <div className="col-md-9">{product.spec.label}</div>
+        <div className="col-md-3 text-secondary">Description</div>
+        <div className="col-md-9">{product.spec.description}</div>
+        <div className="col-md-3 text-secondary">Icon URL</div>
+        <div className="col-md-9">{product.spec.icon}</div>
+        <div className="col-md-3 text-secondary">Overview URL</div>
+        <div className="col-md-9">{product.spec.overviewComponentUrl}</div>
+      </div>
+    </>
   }
   
+  const NoSuchProduct = () => (
+    <div className="mt-5 text-center text-secondary">No, there is no such product.</div>
+  )
+
   return (
     <div className="p-4">
       {
         products.loading ? <Loading /> : 
         products.error ? <LoadingError error = { products.error }/> :  
-        products.data ? <Product name={name}/> : 
-        <NoSuchProduct />
+        products.data ? <Product name={name}/> : <NoSuchProduct />
       }
     </div>  
   )
