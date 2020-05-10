f0d2a503c045f007b24912f9eae518465b730368
Jaroslav Psenicka <jaroslav.psenicka@gmail.com>
2020-05-10 23:11:06 +0200

    private stuff

--- server/config.js
+++ server/config.js
@@ -50,9 +50,9 @@ module.exports = {
     },
   },
   assistant: {
-    id: process.env.ASSISTANT_ID || '1e9c2251-7e4a-4890-975b-0fc5d6e58a8e',
-    apikey: process.env.ASSISTANT_API_KEY || 'j91kezi1_0uC8RQewlxCarIgHokd3pzJQLqIQA-l7Uik',
-    url: process.env.ASSISTANT_URL || 'https://api.eu-de.assistant.watson.cloud.ibm.com/instances/3964d5bd-64e6-4d13-bb69-d8c49ec6fd57',
+    id: process.env.ASSISTANT_ID,
+    apikey: process.env.ASSISTANT_API_KEY,
+    url: process.env.ASSISTANT_URL,
     version: '2020-04-01'
   }
 };
